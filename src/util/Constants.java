08e6434d385526bb7091d3885c5c141741522d3e
Raghavan <itsrag@gmail.com>
2014-04-21 20:54:58 -0500

    Updating constants

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -1,25 +1,31 @@
 package util;
 
-
 public interface Constants {
 
-	//Raghavan settings
+	// Raghavan settings
 	String ENGLISH_PCFG_DATASET = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
 	String TREE_OUTPUT_FILE = "data/tree.txt";
 	String DATA_INPUT_FILE = "data/input-data.txt";
 	String ASSIGNMENT_INPUT_FILE = "data/assignment-3a";
 	String ASSIGNMENT_OUTPUT_FILE = "data/assignment-3a-output";
 	String ASSIGNMENT_QUESTIONS_INPUT = "data/inputQuestions.txt";
-	
-	//Mysql Db settings
+
+	// Mysql Db settings
 	final String dbDriver = "com.mysql.jdbc.Driver";
-	
-	/*final String mydata_url = "jdbc:mysql://proto.rrc.uic.edu:33333/RRC_USERS";
-	String mydata_username = "jdbcconnector";
-	String mydata_password = "password12345";*/
-	
 	final String mydata_url = "jdbc:mysql://localhost:3306/sochi";
 	String mydata_username = "root";
 	String mydata_password = "encrypted";
+
+	// sqlite Db settings
+	final String sqlite_dbDriver = "org.sqlite.JDBC";
+	final String sqlite_url = "jdbc:sqlite:sochi";
+	String sqlite_username = "root";
+	String sqlite_password = "encrypted";
+
+	// Query
+	String GET_ALL_COUNTRIES = "select nationality from athletes;";
+	String GET_SOCHI_RESULT = "select  a.name as player_name,a.nationality as player_nationality,a.gender as player_gender,"
+			+ "c.name competition_name,c.type as competition_type,r.medal as medal from athletes a, competitions c,results r"
+			+ " where c.comp_id = r.comp_id  and a.name = r.winner ";
 
 }
